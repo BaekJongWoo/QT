a47728aa84d2f4a3e8ef4e2e9b400c9dd572a8cb
종우 백 <jaden123456@naver.com>
2024-09-30 15:13:03 +0900

    update

--- simulator/Circuit.py
+++ simulator/Circuit.py
@@ -1,26 +1,31 @@
 from Module import *
 
 class QuantumCircuit():
-    def __init__(self, input_num, output_num):
-        self.input = [Qbit() for _ in range(input_num)]
-        self.output = [Qbit() for _ in range(output_num)]
-
+    def __init__(self, size):
+        self.size = size
+        self.value = self.getInit(size)
         self.gates = []
     
-    def add(self, new_modules):
+    def getInit(self, size):
+        zeroket = np.array([[complex(1,0)],[complex(0,0)]])
+        if size <= 1:
+            return zeroket
+        return np.kron(zeroket, self.getInit(size-1))
+
+    def add(self, modules, idx):
+        new_modules = modules(idx, self.size)
         self.gates.append(new_modules)
 
     def run(self):
         for gate in self.gates:
-            gate.run(self)
+            self.value = np.dot(gate.gate, self.value)
 
     def draw(self):
         pass
 
 
 if __name__ == "__main__":
-    qc = QuantumCircuit(1,0)
-    qc.add(X(0))
-    qc.add(Hadmard(0))
+    qc = QuantumCircuit(3)
+    qc.add(X, 1)
     qc.run()
-    qc.input[0].print()
+    print(qc.value)
--- simulator/Module.py
+++ simulator/Module.py
@@ -1,48 +1,73 @@
-from Qbit import Qbit
-from Circuit import QuantumCircuit
 import numpy as np
 
-class Module():
-    def __init__(self, idx:int) -> None:
+from abc import ABC, abstractmethod
+
+class Module(ABC):
+    def __init__(self, idx:int, size:int) -> None:
         self.idx = idx
+        self.size = size
+        self.gate = self.Gate()
 
-    def run(self, circuit: QuantumCircuit):
-        qbit = circuit.input[self.idx]
-        qbit.value = np.dot(self.gate, qbit.value)
-
-class ControlledModule(Module):
-    def __init__(self, control_idx:int, idx:int) -> None:
-        self.control_idx = control_idx
-        super().__init__(idx)
-
-    def run(self, circuit: QuantumCircuit):
-        control_qbit = circuit.input[self.control_idx]
-        qbit = circuit.input[self.idx]
-        
-        value = np.dot(self.gate, np.concatenate(control_qbit.value, qbit.value))
-        control_qbit.value, qbit.value = np.split(value, 2)
+    @abstractmethod
+    def Gate(self):
+        pass
+    
 
 class X(Module):
-    def __init__(self, idx: int) -> None:
-        super().__init__(idx)
-        self.gate = np.array([[0,1],[1,0]])
+
+    X = np.array([[0,1],[1,0]])
+    I = np.array([[1,0],[0,1]])
+
+    def Gate(self):
+        gate = 1
+        for idx in range(self.size):
+            if (idx == self.idx):
+                gate = np.kron(self.X , gate)
+            else:
+                gate = np.kron(self.I, gate)
+        return gate
+
+class Y(Module):
+
+    Y = np.array([[0, complex(0,-1)],[complex(0,1),0]])
+    I = np.array([[1,0],[0,1]])
+
+    def Gate(self):
+        gate = 1
+        for idx in range(self.size):
+            if (idx == self.idx):
+                gate = np.kron(self.Y , gate)
+            else:
+                gate = np.kron(self.I, gate)
+        return gate
 
 class Z(Module):
-    def __init__(self, idx: int) -> None:
-        super().__init__(idx)
-        self.gate = np.array([[1,0],[0,-1]])
 
-class Hadmard(Module):
-    def __init__(self, idx: int) -> None:
-        super().__init__(idx)
-        self.gate = np.array([[1,1],[1,-1]]) / np.sqrt(2)
+    Z = np.array([[1,0],[0,-1]])
+    I = np.array([[1,0],[0,1]])
 
-class CX(ControlledModule):
-    def __init__(self, control_idx: int, idx: int) -> None:
-        super().__init__(control_idx, idx)
-        self.gate = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]])
-        
-class CZ(ControlledModule):
-    def __init__(self, control_idx: int, idx: int) -> None:
-        super().__init__(control_idx, idx)
-        self.gate = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,-1]])
+    def Gate(self):
+        gate=1
+        for idx in range(self.size):
+            if (idx == self.idx):
+                gate = np.kron(self.Z , gate)
+            else:
+                gate = np.kron(self.I, gate)
+        return gate
+
+class Madamard(Module):
+
+    H = np.array([[1,1],[1,-1]]) / np.sqrt(2)
+    I = np.array([[1,0],[0,1]])
+
+    def Gate(self):
+        gate=1
+        for idx in range(self.size):
+            if (idx == self.idx):
+                gate = np.kron(self.Z , gate)
+            else:
+                gate = np.kron(self.I, gate)
+        return gate
+
+if __name__ == "__main__":
+    print(Y(0, 3).gate)
